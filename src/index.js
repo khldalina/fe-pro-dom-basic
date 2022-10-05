cca41c00e101b1d3b4f4c3d9c4263cb74eebb4f7
Alina <khldalina@gmail.com>
2022-10-05 23:26:06 +0300

    added js code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-export const paintCards = () => {};
+export const paintCards = () => {
+    let listOfLi = document.getElementsByTagName("li");
+    for (let i = 0; i < listOfLi.length; i++) {
+        if (!(i % 2)) {
+            listOfLi[i].style.backgroundColor = "red";
+        }
+    }
+}
 
-export const findElement = () => {};
+
+    export const findElement = () => {
+    let likeLi = document.getElementsByClassName('likedItem');
+    for (let i = 0; i < likeLi.length; i++){
+        likeLi[i].style.backgroundColor = "blue";
+    }
+};
+
